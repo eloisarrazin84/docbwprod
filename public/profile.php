91baa4e8c4abeecbfa9ac820b26da4a9d459a585
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-11-22 14:19:44 +0100

    Update profile.php

--- public/profile.php
+++ public/profile.php
@@ -88,6 +88,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Profil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <style>
         body {
             background-color: #f8f9fa;
